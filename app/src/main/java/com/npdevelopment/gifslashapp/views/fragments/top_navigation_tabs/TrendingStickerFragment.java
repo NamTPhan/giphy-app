f860260841f74406ee9c80e150124e96f144f555
Nam <namtphann@gmail.com>
2021-07-30 14:42:05 +0200

    Migration AndroidX fragments and viewpager

--- app/src/main/java/com/npdevelopment/gifslashapp/views/fragments/top_navigation_tabs/TrendingStickerFragment.java
+++ app/src/main/java/com/npdevelopment/gifslashapp/views/fragments/top_navigation_tabs/TrendingStickerFragment.java
@@ -1,18 +1,18 @@
 package com.npdevelopment.gifslashapp.views.fragments.top_navigation_tabs;
 
-import android.arch.lifecycle.Observer;
-import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import com.bumptech.glide.Glide;
 import com.npdevelopment.gifslashapp.R;
